2efebb00235f62b27961161ea7332b11fbc664a9
AnderSpidy <andersonreinaldo2347@gmail.com>
2022-06-14 09:16:09 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/projetocassino/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.projetocassino;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        TelaPrincipal iniciar = new TelaPrincipal();
+        iniciar.setVisible(true);
+    }
+        // TODO code application logic here
+    }
+   
