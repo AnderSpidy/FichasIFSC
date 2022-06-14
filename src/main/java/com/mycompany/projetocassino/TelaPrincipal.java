ac2174904a62aac620075671b6c70f924c4af868
Luan Vale <luancabral5566@gmail.com>
2022-06-14 13:44:08 -0300

    feat: adicionado botao desabilitado caso nao tenha jogadores no menu principal // fix: corrigido erro no combobox de carregar nome dos jogadores.

--- src/main/java/com/mycompany/projetocassino/TelaPrincipal.java
+++ src/main/java/com/mycompany/projetocassino/TelaPrincipal.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.projetocassino;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Aluno
@@ -13,8 +15,12 @@ public class TelaPrincipal extends javax.swing.JFrame {
     /**
      * Creates new form TelaPrincipal
      */
-    public TelaPrincipal() {
+    public TelaPrincipal(ArrayList<Jogador> jogadores) {
         initComponents();
+        if(jogadores.isEmpty()){
+            btnAdicionarFichas.setEnabled(false);
+            btnVisualizarJogadores.setEnabled(false);
+        }
     }
 
     /**
@@ -97,40 +103,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
         telaJogador.setVisible(true);
     }//GEN-LAST:event_btnAdicionarJogadorMouseClicked
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TelaPrincipal().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdicionarFichas;
